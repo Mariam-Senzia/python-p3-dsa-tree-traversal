a9ecf7e6c453f929eeab5aecc353f0d864c778d3
Mariam Senzia <mariamsenzia@gmail.com>
2024-03-13 22:46:11 +0300

    dsa tree

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,15 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+     return self._dfs_search(self.root, id)
+  
+  def _dfs_search(self, node, target_id):
+        if node is None:
+            return None
+        if node['id'] == target_id:
+            return node
+        for child in node['children']:
+            result = self._dfs_search(child, target_id)
+            if result:
+                return result
+        return None
